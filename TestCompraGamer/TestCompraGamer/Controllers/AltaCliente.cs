7ab621d31a4f824fa2fc17e5c57fd593cff44717
Tomás Bernardin <tomibernardin@gmail.com>
2023-06-01 02:30:07 -0300

    Comienzo de la API con sus endpoints

---
+++ TestCompraGamer/TestCompraGamer/Controllers/AltaCliente.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestCompraGamer.Models;
+
+namespace TestCompraGamer.Controllers
+{
+    [ApiController]
+    [Route("cliente")]
+    public class AltaCliente : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarCliente()
+        {
+            List<Cliente> clientes = new List<Cliente>
+            {
+                new Cliente
+                {
+                    id = 1,
+                    nombre = "Jesus"
+                },
+                new Cliente
+                {
+                    id = 2,
+                    nombre = "Miguel"
+                },
+                new Cliente
+                {
+                    id = 3,
+                    nombre = "Luis"
+                }
+            };
+        return clientes;
+        }
+
+        [HttpGet]
+        [Route("listarxid")]
+        public dynamic listarClienteXId(int id)
+        {
+            return new Cliente
+            {
+                id = id,
+                nombre = "Jesus"
+            };
+        }
+
+        //[HttpGet]
+        //[Route("listarxdni")]
+        //public dynamic listarClienteXdni(int dni)
+        //{
+        //    return new Cliente();
+        //}
+
+        [HttpPost]
+        [Route("guardar")]
+        public dynamic guardarCliente(Cliente cliente)
+        {
+            cliente.id = 4;
+            return new
+            {
+                success = true,
+                message = "Cliente registrado",
+                result = cliente
+            };
+        }
+
+        [HttpDelete]
+        [Route("eliminar")]
+        public dynamic eliminarCliente(Cliente cliente)
+        {
+            string token = Request.Headers.Where(x => x.Key == "token").FirstOrDefault().Value;
+            if (token != "testtoken")
+            {
+                return new
+                {
+                    success = false,
+                    message = "token incorrecto",
+                    result = ""
+                };
+            }
+            return new
+            {
+                success = true,
+                message = "cliente eliminado",
+                result = cliente
+            };
+        }
+    }
+}
---
+++ TestCompraGamer/TestCompraGamer/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace TestCompraGamer.Models
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        /*public string apellido { get; set;}
+        public DateTime fecha_nacimiento { get; set;}
+        public string direccion { get; set; }
+        public int telefono { get; set; }
+        public string email { get; set; }
+        public int dni { get; set; }
+        public int cuit { get; set; }*/
+    }
+}
